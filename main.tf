280bc43db35a53bf67af6acb7d7cdbca7da21f21
Serhii Chekez <serhii.cherkez@gmail.com>
2024-05-01 15:49:35 +0000

    fix

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/serhii-cherkez/tf-google-gke-cluster"
+  source         = "github.com/serhii-cherkez/tf-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
